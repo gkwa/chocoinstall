b1d3cc02c119966db3f4667ebd5658857e7e8779
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-02-13 23:02:34 -0800

    Not using external wget, add alias back

--- chocoinstall.ps1
+++ chocoinstall.ps1
@@ -1,7 +1,3 @@
-if (test-path Alias:\wget) {
-	Remove-Item Alias:\wget
-}
-
 # .Net 4.6.2:
 $url="https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe"
 $outfile="NDP462-KB3151800-x86-x64-AllOS-ENU.exe"
