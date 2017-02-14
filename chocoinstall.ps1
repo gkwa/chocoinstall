a9de87c38f97260206121c90a3cde19bb4a673dd
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-02-13 23:22:19 -0800

    Prevent downloading multiple times

--- chocoinstall.ps1
+++ chocoinstall.ps1
@@ -10,13 +10,17 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercon
 # .Net 4.6.2:
 $url="https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe"
 $outfile="NDP462-KB3151800-x86-x64-AllOS-ENU.exe"
-(new-object System.Net.WebClient).DownloadFile($url, $outfile)
+if(!(test-path $outfile)){
+	(new-object System.Net.WebClient).DownloadFile($url, $outfile)
+}
 cmd /c start /wait ./NDP462-KB3151800-x86-x64-AllOS-ENU.exe /q
 
 # Windows Management Framework (WMF) 5.0
 $url="https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win7-KB3134760-x86.msu"
 $outfile="Win7-KB3134760-x86.msu"
-(new-object System.Net.WebClient).DownloadFile($url, $outfile)
+if(!(test-path $outfile)){
+	(new-object System.Net.WebClient).DownloadFile($url, $outfile)
+}
 cmd /c start /wait ./NDP462-KB3151800-x86-x64-AllOS-ENU.exe /q
 
 mkdir KB3134760
